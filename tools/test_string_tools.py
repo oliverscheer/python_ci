d00591d6a95feca4e3ac5dd90d5dc2ae95a33b66
Oliver Scheer <oliver.scheer@microsoft.com>
2021-05-26 19:30:44 +0000

    removed failing test

--- tools/test_string_tools.py
+++ tools/test_string_tools.py
@@ -10,6 +10,6 @@ def test_lower_case():
     assert st.lower() == "hello world"
 
 
-def test_lower_case_fail():
-    st = string_tools.StringTools("Hello world")
-    assert st.lower() == "hello WORLD"
+# def test_lower_case_fail():
+#     st = string_tools.StringTools("Hello world")
+#     assert st.lower() == "hello WORLD"
