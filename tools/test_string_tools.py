3286e62a3553be7e6cd9eafe2b5f3bdd0c288ee6
Oliver Scheer <oliver.scheer@microsoft.com>
2021-05-26 21:19:31 +0200

    add license

--- tools/test_string_tools.py
+++ tools/test_string_tools.py
@@ -1,10 +1,10 @@
-from tools.string_tools import StringTools
+from tools import string_tools
 
 def test_upper_case():
-    st = StringTools("Hello world")
+    st = string_tools.StringTools("Hello world")
     assert st.upper() == 'HELLO WORLD'
 
     
 def test_lower_case():
-    st = StringTools("Hello world")
+    st = string_tools.StringTools("Hello world")
     assert st.lower() == "hello world"
