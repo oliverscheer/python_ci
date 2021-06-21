d67c379440d1873a753c47e7031bb9564d96de21
Oliver Scheer <oscheer@microsoft.com>
2021-06-21 11:33:11 +0000

    add: pylint

---
+++ tools/math_tools.py
@@ -0,0 +1,11 @@
+""" some math tools """
+
+class MathTools:
+    """ some math tools """
+
+    def add(a, b):
+        """ add two values """
+        return a + b
+
+    def sub(a, b):
+        """ subtract two values """
